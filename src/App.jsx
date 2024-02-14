8a0ca14dc006c71d81e5ac864f16f075efdebea8
Efraim Nabil <efraimefefraim@gmail.com>
2024-02-14 17:10:24 +0200

    Install Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-    return <div data-testid='app'></div>;
+    return (
+        <div data-testid='app'>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
